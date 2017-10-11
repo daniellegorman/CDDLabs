309037718fd3affdef5ee128f8e5766600d7f0ea
daniellegorman <gormandanielle13@gmail.com>
2017-10-11 18:06:20 +0100

    Add files via upload
    Second Commit!

---
+++ Lab1/signal.cpp
@@ -0,0 +1,36 @@
+/*Author: Danielle Gorman
+  Date: 9/10/17
+  Description: Main Program for Semaphore to run. This program uses 
+  semaphores to run task one first, and task two second."
+*/
+
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+
+void taskOne(std::shared_ptr<Semaphore> theSemaphore){
+  std::cout <<"I ";
+  std::cout << "must ";
+  std::cout << "print ";
+  std::cout << "first"<<std::endl;
+  theSemaphore->Signal();
+}
+void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
+  theSemaphore->Wait();
+  std::cout <<"This ";
+  std::cout << "will ";
+  std::cout << "appear ";
+  std::cout << "second"<<std::endl;
+}
+
+int main(void){
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> sem( new Semaphore);
+  /**< Launch the threads  */
+  threadOne=std::thread(taskTwo,sem);
+  threadTwo=std::thread(taskOne,sem);
+  std::cout << "Launched from the main\n";
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
